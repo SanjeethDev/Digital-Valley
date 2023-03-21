bda6e67553b807c245b0bfc38caa52975e46c2a6
SanjeethDev <91370632+SanjeethDev@users.noreply.github.com>
2023-03-21 17:21:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bankapp/Essential_RecyclerViewAdapter.java
@@ -0,0 +1,52 @@
+package com.example.bankapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class Essential_RecyclerViewAdapter extends RecyclerView.Adapter<Essential_RecyclerViewAdapter.MyViewHolder> {
+    Context context;
+    ArrayList<EssentialModel> essentialModels;
+    public Essential_RecyclerViewAdapter(Context context, ArrayList<EssentialModel> essentialModels) {
+        this.context = context;
+        this.essentialModels = essentialModels;
+    }
+
+    @NonNull
+    @Override
+    public Essential_RecyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.essentials_item_block,parent,false);
+        return new Essential_RecyclerViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Essential_RecyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.imageView.setImageResource(essentialModels.get(position).getItem_image());
+        holder.txtTitle.setText(essentialModels.get(position).getItem_name());
+        holder.txtValue.setText(essentialModels.get(position).getItem_value());
+    }
+
+    @Override
+    public int getItemCount() {
+        return essentialModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView txtTitle, txtValue;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            txtTitle = itemView.findViewById(R.id.textView);
+            txtValue = itemView.findViewById(R.id.textView2);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bankapp/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.bankapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<EssentialModel> esstential_array = new ArrayList<>();
+    ArrayList<RecentTransactionModel> recentTLog_array = new ArrayList<>();
+    int[] essential_images = {R.drawable.ic_deposit, R.drawable.ic_loan, R.drawable.ic_savings};
+
+    ToggleButton hide_button;
+    Essential_RecyclerViewAdapter ERVAdapter;
+    RecentTranscation_RVAdapter RTRVAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView essentialsrecyclerView = findViewById(R.id.estn_recyclerview);
+        hideEsstentialModel();
+        ERVAdapter = new Essential_RecyclerViewAdapter(this, esstential_array);
+        essentialsrecyclerView.setAdapter(ERVAdapter);
+        essentialsrecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
+        RecyclerView recentTranscationsView = findViewById(R.id.recent_transcations);
+        setRecentTranscationData();
+
+
+        hide_button = findViewById(R.id.hide_essentials);
+        hide_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hide_button.isChecked()) {
+                    setEssentialModel();
+                    ERVAdapter.notifyDataSetChanged();
+                } else {
+                    hideEsstentialModel();
+                    ERVAdapter.notifyDataSetChanged();
+                }
+            }
+        });
+    }
+
+    private void setRecentTranscationData() {
+        String[] rtlog_names = getResources().getStringArray(R.array.recenttlog_names);
+        String[] rtlog_date = getResources().getStringArray(R.array.recenttlog_date);
+        String[] rtlog_amount = getResources().getStringArray(R.array.recenttlog_amount);
+        String[] rtlog_type = getResources().getStringArray(R.array.recenttlog_type);
+
+        for (int i=0; i<rtlog_names.length; i++) {
+            recentTLog_array.add(new RecentTransactionModel(rtlog_names[i], rtlog_date[i], rtlog_amount[i], rtlog_type[i]));
+        }
+
+
+
+    }
+
+    private void setEssentialModel() {
+        String[] essential_values = getResources().getStringArray(R.array.essentialcard_values);
+        String[] essential_titles = getResources().getStringArray(R.array.essentialcard_names);
+        esstential_array.clear();
+        for (int i=0; i<essential_titles.length; i++) {
+            esstential_array.add(new EssentialModel(essential_images[i], essential_titles[i], essential_values[i]));
+        }
+    }
+
+    private void hideEsstentialModel() {
+        String[] essential_titles = getResources().getStringArray(R.array.essentialcard_names);
+        esstential_array.clear();
+        for (int i=0; i<essential_titles.length; i++) {
+            esstential_array.add(new EssentialModel(essential_images[i], essential_titles[i], "XXXX"));
+        }
+    }
+}
